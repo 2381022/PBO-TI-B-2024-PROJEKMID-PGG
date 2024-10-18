2de562364eae044a54de3713d798268e3e542f28
Gior Sheva Rotinsulu <2381042@unai.edu>
2024-10-18 10:04:37 +0700

    18/10/2024 Menambahkan pengulangan di method main dan menambahkan fitur pemilihan menu

--- ProjectMid/MenuFIle.java
+++ ProjectMid/MenuFIle.java
@@ -6,7 +6,24 @@ public class MenuFIle {
     private static Scanner input = new Scanner(System.in);
     
     public static void main(String[] args) {
-        tampilkanMenu();
+        while(true) {
+            tampilkanMenu();
+            int pilih = input.nextInt();
+            switch (pilih) {
+                case 1:
+                    customerFile.menuKlien();
+                    break;
+                case 2:
+                    adminFile.menuAdmin();
+                    break;
+                case 3:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Maaf Menu Tidak Tersedia");
+                    break;
+            }
+        }
 
 
     }
@@ -14,7 +31,7 @@ public class MenuFIle {
     private static void tampilkanMenu() {
         System.out.println("1. Pelanggan");
         System.out.println("2. Admin");
+        System.out.println("3. Keluar Aplikasi");
         System.out.print("Masukkan Pilihan Anda:");
     }
-
 }
