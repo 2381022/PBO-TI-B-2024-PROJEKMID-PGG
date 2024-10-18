8a5c22468621fbcfc86e14a137fd6e4052d44e61
Gior Sheva Rotinsulu <2381042@unai.edu>
2024-10-18 09:51:47 +0700

    18/10/2024 Menambahkan tampilkan menu

--- ProjectMid/MenuFIle.java
+++ ProjectMid/MenuFIle.java
@@ -1,5 +1,20 @@
 package ProjectMid;
 
+import java.util.Scanner;
+
 public class MenuFIle {
+    private static Scanner input = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        tampilkanMenu();
+
+
+    }
+
+    private static void tampilkanMenu() {
+        System.out.println("1. Pelanggan");
+        System.out.println("2. Admin");
+        System.out.print("Masukkan Pilihan Anda:");
+    }
 
 }
